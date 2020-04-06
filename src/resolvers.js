a91ff7a9e7afeddf360713ee1e38c91cab205c7c
Camilo Sepulveda <csepulvedaz@unal.edu.co>
2020-04-06 18:02:27 -0500

    :zap::sparkles: Se creo schema driver y vehicle, con sus respecticos typedefs y resolvers, agregando al relacion 1-1

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,4 +1,6 @@
 import User from "./models/User";
+import Driver from "./models/Driver";
+import Vehicle from "./models/Vehicle";
 
 export const resolvers = {
     Query: {
@@ -7,6 +9,7 @@ export const resolvers = {
         },
     },
     Mutation: {
+        //Create User
         async createUser(_, { input }) {
             const newUser = new User(input);
             await newUser.save();
@@ -17,6 +20,45 @@ export const resolvers = {
         },
         async updateUser(_, { _id, input }) {
             return await User.findByIdAndUpdate(_id, input, { new: true });
+        },
+
+        //Create Driver
+        async createDriver(_, { input }) {
+            const newDriver = new Driver(input);
+            await newDriver.save();
+            return newDriver;
+        },
+        async deleteDriver(_, { _id }) {
+            return await Driver.findByIdAndDelete(_id);
+        },
+        async updateDriver(_, { _id, input }) {
+            return await Driver.findByIdAndUpdate(_id, input, { new: true });
+        },
+
+        //Create Vehicle
+        async createVehicle(_, { input }) {
+            const newVehicle = new Vehicle(input);
+            let createdVehicle;
+            return await newVehicle
+                .save()
+                .then(async (result) => {
+                    createdVehicle = {
+                        ...result._doc,
+                        _id: result._doc._id.toString(),
+                    };
+                    return await Driver.findById(input.idDriver);
+                })
+                .then(async (driver) => {
+                    driver.idVehicle = newVehicle;
+                    return await driver.save();
+                })
+                .then(() => createdVehicle);
+        },
+        async deleteVehicle(_, { _id }) {
+            return await Vehicle.findByIdAndDelete(_id);
+        },
+        async updateVehicle(_, { _id, input }) {
+            return await Vehicle.findByIdAndUpdate(_id, input, { new: true });
         },
     },
 };
--- src/schema.js
+++ src/schema.js
@@ -10,25 +10,73 @@ const typeDefs = `
     type User {
         _id: Int!
         name: String!
-        surname: String!,
+        surname: String!
         phone: Int!
         email: String!
         password: String!
+    }
+
+    type Driver {
+        _id: Int!
+        name: String!
+        surname: String!
+        phone: Int!
+        email: String!
+        password: String!
+    }
+
+    type Vehicle {
+        _id: String!
+        brand: String!
+        model: String!
+        year: Int!
+        type: String!
+        dimensions: String!
+        capacity: Int!
+        commentary: String!
+        idDriver: Int!
     }
 
     type Mutation {
         createUser(input: userInput): User
         deleteUser(_id: Int): User
         updateUser(_id: Int, input: userInput): User
+        createDriver(input: driverInput): Driver
+        deleteDriver(_id: Int): Driver
+        updateDriver(_id: Int, input: driverInput): Driver
+        createVehicle(input: vehicleInput): Vehicle
+        deleteVehicle(_id: String): Vehicle
+        updateVehicle(_id: String, input: vehicleInput): Vehicle
     }
 
     input userInput {
         _id: Int!
         name: String!
-        surname: String!,
+        surname: String!
         phone: Int!
         email: String!
         password: String!
+    }
+
+    input driverInput {
+        _id: Int!
+        name: String!
+        surname: String!
+        phone: Int!
+        email: String!
+        password: String!
+    }
+
+    input vehicleInput {
+        _id: String!
+        brand: String!
+        model: String!
+        year: Int!
+        type: String!
+        dimensions: String!
+        capacity: Int!
+        commentary: String!
+        idDriver: Int!
     }
 
 `;
