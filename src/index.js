e3b89f118c4a2bebba46bb2d0979ffa31ef44df6
Camilo Sepulveda <csepulvedaz@unal.edu.co>
2020-05-07 12:23:03 -0500

    :zap:Configuracion puerto

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import "./database";
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
